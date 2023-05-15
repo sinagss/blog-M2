da67ccef3df6b94080acd73f96786cfdc0eb1da4
sinagss <sina.ghassai@gmail.com>
2023-05-15 20:21:59 +0330

    added application paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  createBrowserRouter,
+  Navigate,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import DrawerAppBar from "./components/DrawerAppBar";
+import About from "./pages/About";
+import Users from "./pages/Users";
+import Login from "./pages/Login";
+import AuthProvider from "./context/AuthProvider";
+import { usePersistedState } from "./hooks/usePersistedState";
+import { keys } from "./constants/storageKeys";
+import Container from "@mui/material/Container";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLoggedIn] = usePersistedState(keys.isLoggedIn, false);
+  const [accessLevel] = usePersistedState(keys.accessLevel, 2);
+
+  //todo: states are not getting updated so pathing is not working
+  const router = createBrowserRouter([
+    {
+      path: "",
+      element: (
+        <>
+          <DrawerAppBar />
+          <Container>
+            <Outlet />
+          </Container>
+        </>
+      ),
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "posts", element: <Home /> },
+        {
+          path: "users",
+          element:
+            isLoggedIn && accessLevel === 0 ? (
+              <Users />
+            ) : (
+              <Navigate to="/" replace />
+            ),
+        },
+        { path: "about", element: <About /> },
+        {
+          path: "login",
+          element: isLoggedIn ? <Navigate to="/" replace /> : <Login />,
+        },
+      ],
+    },
+  ]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
