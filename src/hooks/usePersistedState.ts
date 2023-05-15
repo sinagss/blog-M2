90e55fbaf7b525e172088944eee8f8d9b9727006
sinagss <sina.ghassai@gmail.com>
2023-05-15 20:24:29 +0330

    created hook to save sate to local storage

---
+++ src/hooks/usePersistedState.ts
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+export function usePersistedState<T>(
+  key: string,
+  initialValue: T
+): [T, (value: T) => void] {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: T) => {
+    try {
+      setStoredValue(value);
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const item = localStorage.getItem(key);
+    if (item) {
+      setStoredValue(JSON.parse(item));
+    }
+  }, [key]);
+
+  return [storedValue, setValue];
+}
