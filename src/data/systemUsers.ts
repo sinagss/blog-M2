8e15497f75e0374d2d7c9a1f15c79d9f9e674269
sinagss <sina.ghassai@gmail.com>
2023-05-15 20:24:03 +0330

    defigned system users as constants

---
+++ src/data/systemUsers.ts
@@ -0,0 +1,17 @@
+export const systemUsers: SystemUser[] = [
+  {
+    username: "admin",
+    password: "admin",
+    accessLevel: 0,
+  },
+  {
+    username: "test1",
+    password: "test1",
+    accessLevel: 1,
+  },
+  {
+    username: "test2",
+    password: "test2",
+    accessLevel: 2,
+  },
+];
