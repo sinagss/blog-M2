8f1fce15801acbebd91fd35fc6d9aaec773f2a3b
sinagss <sina.ghassai@gmail.com>
2023-05-18 13:12:07 +0330

    Implemented PostItem

---
+++ src/components/PostItem.tsx
@@ -0,0 +1,99 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Skeleton from "@mui/material/Skeleton";
+import Stack from "@mui/material/Stack";
+import CoverImg from "../assets/contemplative-reptile.jpg";
+
+interface Props {
+  loading?: boolean;
+  data: Post;
+}
+
+// type Props = {
+//   loading?: boolean;
+//   data: Post;
+// };
+
+function PostItem(props: Props) {
+  const { loading = false } = props;
+
+  return (
+    <Card sx={{ maxWidth: 345, m: 2 }}>
+      <CardHeader
+        avatar={
+          loading ? (
+            <Skeleton
+              animation="wave"
+              variant="circular"
+              width={40}
+              height={40}
+            />
+          ) : (
+            <Avatar alt={props.data.title} src={CoverImg} />
+          )
+        }
+        action={
+          loading ? null : (
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          )
+        }
+        title={
+          loading ? (
+            <Skeleton
+              animation="wave"
+              height={10}
+              width="80%"
+              style={{ marginBottom: 6 }}
+            />
+          ) : (
+            props.data.title
+          )
+        }
+        subheader={
+          loading ? (
+            <Skeleton animation="wave" height={10} width="40%" />
+          ) : (
+            props.data.userId
+          )
+        }
+      />
+      {loading ? (
+        <Skeleton sx={{ height: 190 }} animation="wave" variant="rectangular" />
+      ) : (
+        <CardMedia
+          component="img"
+          height="140"
+          image={CoverImg}
+          alt={props.data.title}
+        />
+      )}
+      <CardContent>
+        {loading ? (
+          <React.Fragment>
+            <Skeleton
+              animation="wave"
+              height={10}
+              style={{ marginBottom: 6 }}
+            />
+            <Skeleton animation="wave" height={10} width="80%" />
+          </React.Fragment>
+        ) : (
+          <Typography variant="body2" color="text.secondary" component="p">
+            {props.data.body}
+          </Typography>
+        )}
+      </CardContent>
+    </Card>
+  );
+}
+
+export default PostItem;
