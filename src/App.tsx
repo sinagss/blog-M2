0a2227699d84c698720e83fab182078df0e53498
sinagss <sina.ghassai@gmail.com>
2023-05-18 13:11:23 +0330

    Added ScrollToTop component to all routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import {
-  createBrowserRouter,
-  Navigate,
-  Outlet,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import DrawerAppBar from "./components/DrawerAppBar";
 import About from "./pages/About";
@@ -13,6 +8,8 @@ import AuthProvider from "./context/AuthProvider";
 import Container from "@mui/material/Container";
 import PrivateRoute from "./routes/PrivateRoute";
 import PublicRoute from "./routes/PublicRoute";
+import Posts from "./pages/Posts";
+import ScrollToTop from "./components/UI/ScrollToTop";
 
 function App() {
   const router = createBrowserRouter([
@@ -23,12 +20,13 @@ function App() {
           <DrawerAppBar />
           <Container>
             <Outlet />
+            <ScrollToTop />
           </Container>
         </>
       ),
       children: [
         { path: "/", element: <Home /> },
-        { path: "posts", element: <Home /> },
+        { path: "posts", element: <Posts /> },
         {
           path: "users",
           element: (
